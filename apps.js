1463f06d09873527fc32e60f3b5e36c61ba8f093
Baked <thatbakedbeans@proton.me>
2025-10-11 19:35:46 -0500

    added de-picker

--- apps.js
+++ apps.js
@@ -5,6 +5,14 @@ const apps = [
     url: "https://oscua.github.io/Sorting-Algorithm-Visualizer/",
     language: "python"
   },
-
+  {
+    name: "DE Picker",
+    description: "Your overengineered solution to picking a Linux Desktop Environment.",
+    url: "https://oscua.github.io/de-picker/",
+    language: "python"
+  },
+  {
+    
+  }
 ];
 
